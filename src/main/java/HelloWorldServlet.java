76b0f7ddcdf2f5868cbc8d3d89cdc5949e8799cf
Jasper Soleymanzadeh <jaspersoleymanzadeh@gmail.com>
2022-03-29 09:37:08 -0500

    finished servlet exercises

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,33 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    private int pageViews;
 
+    @Override
+    public void init() {
+        pageViews = 0;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        PrintWriter out = res.getWriter();
+        String name = req.getParameter("name");
+        boolean reset = Boolean.parseBoolean(req.getParameter("reset"));
+        pageViews++;
+        if (name == null) {
+            name = "World";
+        }
+        if (reset) {
+            init();
+        }
+        res.setContentType("text/html");
+        out.printf("<h1>Hello, %s!</h1>", name);
+        out.printf("<p>Page Views: %d", pageViews);
     }
 }
