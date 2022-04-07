63dc2876780e7fca97f29836fa19b02105c65340
Jasper Soleymanzadeh <jaspersoleymanzadeh@gmail.com>
2022-04-07 14:45:49 -0500

    finished user input exercises

---
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -0,0 +1,53 @@
+package com.codeup.adlister.dao;
+
+import com.codeup.adlister.models.User;
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+
+public class MySQLUsersDao implements Users {
+    private Connection connection;
+
+    public MySQLUsersDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(config.getUrl(), config.getUsername(), config.getPassword());
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database!", e);
+        }
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        String sql = "SELECT * FROM users WHERE username = ?";
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, username);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                return new User(rs.getLong("id"), rs.getString("username"), rs.getString("email"), rs.getString("password"));
+            } else {
+                throw new SQLException("No users matching search term.");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("User search error.", e);
+        }
+    }
+
+    @Override
+    public Long insert(User user) {
+        String sql = "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, user.getPassword());
+            statement.executeUpdate();
+            ResultSet rs = statement.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException("Add user error.", e);
+        }
+    }
+}
